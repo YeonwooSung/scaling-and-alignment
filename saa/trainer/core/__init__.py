252b3b4e1942e21d4a14c2616719d3a05703fb5f
YeonwooSung <neos960518@gmail.com>
2025-02-02 23:51:46 +0900

    rename as 'saa', which stands for scaling-and-alignment

---
+++ saa/trainer/core/__init__.py
@@ -0,0 +1,8 @@
+from .ray_trainer import ResourcePoolManager, Role, contexted_timer
+
+
+__all__ = [
+    "ResourcePoolManager",
+    "Role",
+    "contexted_timer",
+]
---
+++ saa/trainer/core/ray_trainer.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from codetiming import Timer
+from contextlib import contextmanager
+from typing import Dict
+
+from saa.single_controller.ray import RayResourcePool
+
+
+class Role(Enum):
+    """
+    To create more roles dynamically, you can subclass Role and add new members
+    """
+    Actor = 0
+    Rollout = 1
+    ActorRollout = 2
+    Critic = 3
+    RefPolicy = 4
+    RewardModel = 5
+    ActorRolloutRef = 6
+
+
+@dataclass
+class ResourcePoolManager:
+    """
+    Define a resource pool specification. Resource pool will be initialized first.
+    Mapping
+    """
+    resource_pool_spec: dict[str, list[int]]
+    mapping: dict[Role, str]
+    resource_pool_dict: dict[str, RayResourcePool] = field(default_factory=dict)
+
+    def create_resource_pool(self):
+        for resource_pool_name, process_on_nodes in self.resource_pool_spec.items():
+            # max_colocate_count means the number of WorkerGroups (i.e. processes) in each RayResourcePool
+            # For FSDP backend, we recommend using max_colocate_count=1 that merge all WorkerGroups into one.
+            # For Megatron backend, we recommend using max_colocate_count>1 that can utilize different WorkerGroup for differnt models
+            resource_pool = RayResourcePool(
+                process_on_nodes=process_on_nodes,
+                use_gpu=True,
+                max_colocate_count=1,
+                name_prefix=resource_pool_name
+            )
+            self.resource_pool_dict[resource_pool_name] = resource_pool
+
+    def get_resource_pool(self, role: Role) -> RayResourcePool:
+        """Get the resource pool of the worker_cls"""
+        return self.resource_pool_dict[self.mapping[role]]
+
+
+@contextmanager
+def contexted_timer(name: str, timing_raw: Dict[str, float]):
+    with Timer(name=name, logger=None) as timer:
+        yield
+    timing_raw[name] = timer.last
